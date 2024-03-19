62156974159255fdc52ee4930308da90e8a6e2ee
Sajan Gurung <gsajan21@gmail.com>
2024-03-18 21:22:27 -0700

    project folder structure

---
+++ backend/ProductManagementSystem/src/main/java/com/sajan/pms/model/Product.java
@@ -0,0 +1,17 @@
+package com.sajan.pms.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="product")
+public class Product {
+    private Integer productId;
+    private String productName;
+}
