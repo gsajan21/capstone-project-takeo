581fe6cc4dd7fb00efec11412a8f64cd34ee5d9f
Sajan Gurung <gsajan21@gmail.com>
2024-03-21 09:48:23 -0700

    implemented CartServer Layer

--- backend/src/main/java/com/sajan/pms/dto/AddToCartRequest.java
+++ backend/src/main/java/com/sajan/pms/dto/AddToCartRequest.java
@@ -2,8 +2,15 @@ package com.sajan.pms.dto;
 
 import com.sajan.pms.model.Product;
 import com.sajan.pms.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AddToCartRequest {
     private User user;
     private Product product;
--- backend/src/main/java/com/sajan/pms/service/CartService.java
+++ backend/src/main/java/com/sajan/pms/service/CartService.java
@@ -1,6 +1,7 @@
 package com.sajan.pms.service;
 
 import com.sajan.pms.dto.AddToCartRequest;
+import com.sajan.pms.dto.UpdateProductQtyRequest;
 import com.sajan.pms.model.CartItem;
 
 import java.util.Optional;
@@ -9,4 +10,5 @@ import java.util.Optional;
 public interface CartService {
     Optional<CartItem> addToCart(AddToCartRequest addToCartRequest);
     Optional<CartItem> removeFromCartById(Integer cartId);
+    Optional<CartItem> updateQtyOfCartItem(Integer cartId, UpdateProductQtyRequest updateProductQtyRequest);
 }
